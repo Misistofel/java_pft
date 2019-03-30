5542376ae93a50956ccf2d84a56ebfddf0938b6b
Misistofel <marinatofel@gmail.com>
2019-03-30 09:55:50 +0200

    Леция 3.1 If Else

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -11,8 +11,7 @@ public class ContactModificationTests extends TestBase {
     app.getContactHelper().selectContact();
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillAddNewForm(new AddContactData("Margo", "Marina", "Tofel", "Kyiv", "380111111111", "380111", "margotofel@mail.ru", "1", "JANUARY", "1989"));
-
     app.getContactHelper().updateContactModification();
-   // app.getContactHelper().returnToHomePage();
+
   }
 }
