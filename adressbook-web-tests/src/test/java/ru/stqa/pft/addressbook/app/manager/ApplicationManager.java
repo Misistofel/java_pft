8693982870169fcfb994984f1ab552c0308c5de6
Misistofel <marinatofel@gmail.com>
2019-03-31 01:08:43 +0200

    Lessos 3.3 _OK

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/app/manager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/app/manager/ApplicationManager.java
@@ -30,7 +30,7 @@ public class ApplicationManager {
      wd = new InternetExplorerDriver();
    }
 
-    wd.manager().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
     contactHelper=new ContactHelper(wd);
