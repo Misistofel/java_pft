2b6de11f8a3718aaccd7d12d061a98a93d0ef070
Misistofel <marinatofel@gmail.com>
2019-04-07 22:50:54 +0300

    Лекция 3.10. Проверка и обеспечение выполнения предусловий

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -9,6 +9,9 @@ public class GroupModificationTests extends TestBase {
  @Test
   public void testGroupModification (){
    app.getNavigationHelper().gotoGroupPage();
+  if (!app.getGroupHelper().isThereAGroup()) {
+   app.getGroupHelper().createGroup(new GroupData(null, null, "Ma"));
+  }
    app.getGroupHelper().selectGroup();
    app.getGroupHelper().initGroupModification();
    app.getGroupHelper().fillGroupForm(new GroupData("Maria", "Marina", "Ma"));
