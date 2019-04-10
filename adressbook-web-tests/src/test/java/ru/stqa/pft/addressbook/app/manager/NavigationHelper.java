89d4c1f416a13431cce78b09cf837ab9f3493f07
Misistofel <marinatofel@gmail.com>
2019-04-10 16:24:16 +0300

    Revert "Lesson 3.9"
    This reverts commit 94f9b486d5319bd68f2c35834a117496a3fcddb3.

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/app/manager/NavigationHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/app/manager/NavigationHelper.java
@@ -12,17 +12,9 @@ public class NavigationHelper extends HelperBase {
   }
 //проверяем, находимся ли мы на нужной странице:
   public void gotoGroupPage() {
-    if (isElementPresent(By.tagName("h1"))
-            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
-            && isElementPresent(By.name("new"))) {
-      return;
-    }
     click(By.linkText("GROUPS"));
   }
     public void gotoHomePage() {
-    if (isElementPresent(By.id("maintable"))) {
-      return;
-    }
       click(By.linkText("HOME"));
     }
 
